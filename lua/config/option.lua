f386a97299d854e7b4c2c5aacbabf312cb36280d
Ryan Hung <ryanhungryan@gmail.com>
2024-09-06 16:27:19 +0100

    remove pick.lua

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -1,1 +1,8 @@
 vim.g.mapleader = " "
+
+vim.g.loaded_newtrw = 1
+vim.g.loaded_newtrwPlugin = 1
+
+
+vim.opt.relativenumber = true
+vim.opt.smartcase = true
--- lua/pick.lua
+++
@@ -1,9 +0,0 @@
-local M = setmetatable({}, {
-  __call = function(m, ...)
-    return m.wrap(...)
-  end,
-})
-
-function M.config_files()
-  return M.wrap("files", { cwd = vim.fn.stdpath("config") })
-end
